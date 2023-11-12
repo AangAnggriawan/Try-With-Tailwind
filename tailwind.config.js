60da93bf340faf5164fdf2743527117437bcde0a
Aang Anggriawan <aanganggriawan222@gmail.com>
2023-11-12 19:37:04 +0700

    Add

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        ungu: "#5d50c6",
+        pink: "#f85e9f",
+        orange: "#ff5722",
+        gray: "#222831",
+      },
+    },
+  },
+  plugins: [],
+};
